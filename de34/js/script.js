d6d0671945485192f61cea4650b3e1fc17eb8437
tanetaro <r202202256vy@jindai.jp>
2023-06-25 17:46:38 +0900

    20230625ver.1

---
+++ de34/js/script.js
@@ -0,0 +1,32 @@
+$(function(){
+    // 変数navPosに、nav要素の初期位置を代入
+    var navPos = $("nav").offset().top;
+  
+    // ブラウザをスクロール
+    $(window).scroll(function(){
+      // スクロール量とnav要素の初期位置を比較
+      if($(window).scrollTop() > navPos){
+        // 条件を満たした場合：nav要素をブラウザ上部に固定
+        $("nav").css("position", "fixed");
+      }else{
+        // 満たさない場合：nav要素を通常の配置にする
+        $("nav").css("position", "static");
+      }
+    });
+  });
+
+  $(function(){
+      // ナビゲーションをクリック
+      $("a[href^=#])").click(function(){
+            //移動先のコンテンツの位置を習得
+            var target = $($(this).attr("href")).offset().top;
+
+            //70px減らす
+            target -= 500;
+
+            //コンテンツへスクロール
+            $("html,body").animate({scrollTop: target}, 500);
+
+            return false;
+      });
+  });
